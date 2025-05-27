c16dea436b216fa8595a4c9b5e6a7845cf6db9de
Luciano Espiridião <lucianovilasboas@gmail.com>
2025-05-26 21:14:51 -0300

    updates

--- util.py
+++ util.py
@@ -590,7 +590,7 @@ def calcular_metricas_fixar_segunda_sexta(st, df):
     # ============================
     meta_geral = 5000
     meta_mensal = 1500
-    meta_semanal = 400
+    meta_semanal = 200
 
     meta_geral_percentual = (total_cadastros / meta_geral) * 100
     meta_mensal_percentual = (cadastros_mes / meta_mensal) * 100
